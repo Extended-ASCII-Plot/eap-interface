7e36621db96550089ef7ddfed6469fce5d117f99
Found Pan Tiger <renzholy@gmail.com>
2021-10-05 13:31:25 +0800

    Update [value].tsx

--- pages/api/[value].tsx
+++ pages/api/[value].tsx
@@ -1,9 +1,18 @@
+import { ethers } from 'ethers'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function ValueAPI(req: NextApiRequest, res: NextApiResponse) {
+  const { value } = req.query
+  const buf = Buffer.from(
+    ethers.utils.hexZeroPad(ethers.BigNumber.from(value).toHexString(), 32).replace(/^0x/, ''),
+    'hex',
+  )
+
   res.json({
-    name: `${req.query.value}`,
-    description: `${req.query.value}`,
-    image: `http://localhost:3000/${req.query.value}`,
+    name: `${buf.toString('base64')}`,
+    description: `0x${buf.toString('hex')}`,
+    image: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/0x${buf.toString('hex')}`
+      : `http://localhost:3000/0x${buf.toString('hex')}`,
   })
 }
