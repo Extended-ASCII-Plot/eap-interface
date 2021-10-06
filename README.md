387cf2b899cd7e95a8f44cbedec770ee74484715
Found Pan Tiger <renzholy@gmail.com>
2021-10-06 20:33:54 +0800

    add icons

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # Extended ASCII Plot Interface
-
-- https://int10h.org/oldschool-pc-fonts/fontlist/
-- http://datagoblin.itch.io/monogram
-- https://polyducks.itch.io/kitchen-sink-textmode-font
-- https://www.npmjs.com/package/ink
-- https://pixelcons.io/
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,6 +36,49 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="UTF-8" />
           <meta httpEquiv="x-dns-prefetch-control" content="on" />
+
+          <meta name="application-name" content="Extended ASCII Plot" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="Extended ASCII Plot" />
+          <meta name="description" content="Mint your ASCII Art on Ethereum" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#f9f7f1" />
+
+          <link rel="apple-touch-icon" href="/static/icons/touch-icon-iphone.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/static/icons/touch-icon-ipad.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/icons/touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="/static/icons/touch-icon-ipad-retina.png"
+          />
+
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+          <link rel="manifest" href="/static/manifest.json" />
+          <link rel="mask-icon" href="/static/icons/icon-512x512.svg" color="#f9f7f1" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://eap.wtf" />
+          <meta name="twitter:title" content="Extended ASCII Plot" />
+          <meta name="twitter:description" content="Mint your ASCII Art on Ethereum" />
+          <meta
+            name="twitter:image"
+            content="https://eap.wtf/static/icons/android-chrome-192x192.png"
+          />
+          <meta name="twitter:creator" content="@RenzHoly" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Extended ASCII Plot" />
+          <meta property="og:description" content="Mint your ASCII Art on Ethereum" />
+          <meta property="og:site_name" content="Extended ASCII Plot" />
+          <meta property="og:url" content="https://eap.wtf" />
+          <meta property="og:image" content="https://eap.wtf/static/icons/touch-icon-iphone.png" />
         </Head>
         <body>
           <Main />
