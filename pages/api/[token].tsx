005a6c04deab2324184da5b30b9ddc1cec2508b4
Found Pan Tiger <renzholy@gmail.com>
2021-10-06 19:24:08 +0800

    improve api

--- pages/api/[token].tsx
+++ pages/api/[token].tsx
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import base85 from 'base85'
 
 export default async function TokenAPI(req: NextApiRequest, res: NextApiResponse) {
   const { token } = req.query
@@ -12,10 +13,18 @@ export default async function TokenAPI(req: NextApiRequest, res: NextApiResponse
     : `http://localhost:3000/plot/0x${buf.toString('hex')}`
 
   res.json({
-    name: `${buf.toString('base64')}`,
-    description: `0x${buf.toString('hex')}`,
+    name: `${base85.encode(buf)}`,
+    description: `${chunk(buf.toString('hex').toUpperCase().split(''), 16)
+      .map((c16) => c16.join(''))
+      .join('\n')}`,
     image: url,
     external_url: url,
     background_color: '000000',
   })
+}
+
+function chunk<T>(input: T[], size: number): T[][] {
+  return input.reduce((arr, item, idx) => {
+    return idx % size === 0 ? [...arr, [item]] : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]]
+  }, [] as T[][])
 }
