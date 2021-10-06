d3662476d39953e7fad2b9799bd74a0e66935f8f
Found Pan Tiger <renzholy@gmail.com>
2021-10-06 15:10:26 +0800

    env NEXT_PUBLIC_CONTRACT_ADDRESS

--- utils/constants.ts
+++ utils/constants.ts
@@ -92,7 +92,7 @@ export const COLOR = [
   '#FFCCAA',
 ]
 
-export const CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
 
 export const ASCII = BigUint64Array.from([
   0xffffffffffffffffn,
