8e69e2d9c1d14de835fe9792985be1fdb4d528f6
Found Pan Tiger <renzholy@gmail.com>
2021-10-07 03:54:18 +0800

    Update index.tsx

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -5,7 +5,7 @@ export default async function IndexAPI(_req: NextApiRequest, res: NextApiRespons
   res.json({
     name: 'Extended ASCII Plot',
     description: 'Extended ASCII Plot (EAP) is user created ASCII Art',
-    image: `${BASE_URL}static/icons/icon-512x512.svg`,
+    image: `${BASE_URL}static/icons/icon-512x512.png`,
     external_link: BASE_URL,
     seller_fee_basis_points: 100,
     fee_recipient: FEE_RECIPIENT,
