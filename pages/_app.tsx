8560470825b90b25155837098a698bd16ec41c92
Found Pan Tiger <renzholy@gmail.com>
2021-10-10 23:10:10 +0800

    Revert "Update _app.tsx"
    This reverts commit 21642bca6e281fd908d1db4901a97ba2d8a6f19c.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,7 +49,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, viewport-fit=cover"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
         <title>Extended ASCII Plot</title>
       </Head>
