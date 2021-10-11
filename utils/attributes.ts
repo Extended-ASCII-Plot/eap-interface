efedea8ee9f1afcd81e23dc28993ae8dbf050d66
Found Pan Tiger <renzholy@gmail.com>
2021-10-11 09:43:48 +0800

    fix attributes

--- utils/attributes.ts
+++ utils/attributes.ts
@@ -5,9 +5,9 @@ export function differentColors(token: string): number {
     ethers.utils.hexZeroPad(ethers.BigNumber.from(token).toHexString(), 32).replace(/^0x/, ''),
     'hex',
   )
-  const array = new Uint16Array(16)
+  const array = new Uint8Array(16)
   for (let i = 0; i < 16; i++) {
-    array[i] = buf.readUInt16BE(i * 2)
+    array[i] = buf.readUInt8(i * 2 + 1)
   }
   return (
     array
@@ -22,14 +22,9 @@ export function differentCharacters(token: string): number {
     ethers.utils.hexZeroPad(ethers.BigNumber.from(token).toHexString(), 32).replace(/^0x/, ''),
     'hex',
   )
-  const array = new Uint16Array(16)
+  const array = new Uint8Array(16)
   for (let i = 0; i < 16; i++) {
-    array[i] = buf.readUInt16BE(i * 2)
+    array[i] = buf.readUInt8(i * 2)
   }
-  return (
-    array
-      .reduce((prev, dot) => (1 << (((dot & 0xff00) >> 8) + 1)) | prev, 0)
-      .toString(2)
-      .split('1').length - 1
-  )
+  return new Set(array).size
 }
