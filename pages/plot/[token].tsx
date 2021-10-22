3db935e51bdde9f65dc511486f3c8c562c47adde
Found Pan Tiger <renzholy@gmail.com>
2021-10-22 15:21:49 +0800

    Update [token].tsx

--- pages/plot/[token].tsx
+++ pages/plot/[token].tsx
@@ -3,18 +3,26 @@ import { ethers } from 'ethers'
 import { useRouter } from 'next/dist/client/router'
 import useSWR from 'swr'
 import Link from 'next/link'
+import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 import Border from '../../components/border'
 import Dot from '../../components/dot'
 import Plot from '../../components/plot'
 import Text from '../../components/text'
-import { FONT_WIDTH, FONT_SCALE_FACTOR, FONT_HEIGHT, CONTRACT_ADDRESS } from '../../utils/constants'
+import {
+  FONT_WIDTH,
+  FONT_SCALE_FACTOR,
+  FONT_HEIGHT,
+  CONTRACT_ADDRESS,
+  BASE_URL,
+} from '../../utils/constants'
 import { useContract } from '../../contexts/contract-context'
 
 const SCALE = 8
 
-export default function PlotPage() {
+export default function PlotPage(props: { token: string }) {
   const router = useRouter()
-  const { token, from } = router.query as { token?: string; from?: string }
+  const { token = props.token, from } = router.query as { token?: string; from?: string }
   const contract = useContract()
   const { data: index } = useSWR(token ? ['indexByToken', contract.address, token] : null, () =>
     contract.indexByToken(ethers.BigNumber.from(token!)),
@@ -24,87 +32,97 @@ export default function PlotPage() {
   )
 
   return (
-    <div
-      className={css`
-        margin: 0 auto;
-        width: ${(4 * SCALE + 2 + 2) * FONT_WIDTH * FONT_SCALE_FACTOR}px;
-        padding: ${FONT_HEIGHT * FONT_SCALE_FACTOR}px ${FONT_WIDTH * FONT_SCALE_FACTOR}px;
-      `}
-    >
+    <>
+      <Head>
+        <meta property="og:image" content={`${BASE_URL}api/svg/${token}`} />
+        <meta name="twitter:image" content={`${BASE_URL}api/svg/${token}`} />
+      </Head>
       <div
         className={css`
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-        `}
-      >
-        <span>
-          <Link href={from || '/'} passHref={true}>
-            <a>
-              <Text>{Uint8Array.from([0x1b, 0x20])}</Text>
-            </a>
-          </Link>
-          <Text>{`Extended ASCII Plot #${index?.toBigInt().toString() || ''}`}</Text>
-        </span>
-        <a
-          title="OpenSea"
-          href={`https://opensea.io/assets/matic/${CONTRACT_ADDRESS}/${token}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Dot value={0xfa7c} />
-        </a>
-      </div>
-      <div>
-        <Text> </Text>
-      </div>
-      <Border width={4 * SCALE + 2} height={4 * SCALE + 2}>
-        <Plot value={token} scale={SCALE} />
-      </Border>
-      <div>
-        <Text> </Text>
-      </div>
-      <div
-        className={css`
-          width: 100%;
-          display: flex;
-          justify-content: space-between;
+          margin: 0 auto;
+          width: ${(4 * SCALE + 2 + 2) * FONT_WIDTH * FONT_SCALE_FACTOR}px;
+          padding: ${FONT_HEIGHT * FONT_SCALE_FACTOR}px ${FONT_WIDTH * FONT_SCALE_FACTOR}px;
         `}
       >
         <div
           className={css`
-            width: ${16 * FONT_WIDTH * FONT_SCALE_FACTOR}px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
           `}
         >
-          <Text>Data:</Text>
-          <div>
-            <Text> </Text>
-          </div>
-          <Text>
-            {token
-              ? ethers.utils
-                  .hexZeroPad(ethers.BigNumber.from(token).toHexString(), 32)
-                  .replace(/^0x/, '')
-                  .toUpperCase()
-              : undefined}
-          </Text>
+          <span>
+            <Link href={from || '/'} passHref={true}>
+              <a>
+                <Text>{Uint8Array.from([0x1b, 0x20])}</Text>
+              </a>
+            </Link>
+            <Text>{`Extended ASCII Plot #${index?.toBigInt().toString() || ''}`}</Text>
+          </span>
+          <a
+            title="OpenSea"
+            href={`https://opensea.io/assets/matic/${CONTRACT_ADDRESS}/${token}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Dot value={0xfa7c} />
+          </a>
+        </div>
+        <div>
+          <Text> </Text>
+        </div>
+        <Border width={4 * SCALE + 2} height={4 * SCALE + 2}>
+          <Plot value={token} scale={SCALE} />
+        </Border>
+        <div>
+          <Text> </Text>
         </div>
         <div
           className={css`
-            width: ${10 * FONT_WIDTH * FONT_SCALE_FACTOR}px;
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
           `}
         >
-          <Text>Owned by:</Text>
-          <div>
-            <Text> </Text>
+          <div
+            className={css`
+              width: ${16 * FONT_WIDTH * FONT_SCALE_FACTOR}px;
+            `}
+          >
+            <Text>Data:</Text>
+            <div>
+              <Text> </Text>
+            </div>
+            <Text>
+              {token
+                ? ethers.utils
+                    .hexZeroPad(ethers.BigNumber.from(token).toHexString(), 32)
+                    .replace(/^0x/, '')
+                    .toUpperCase()
+                : undefined}
+            </Text>
           </div>
-          <Text>
-            {owner
-              ? ethers.utils.hexZeroPad(owner, 20).replace(/^0x/, '').toUpperCase()
-              : undefined}
-          </Text>
+          <div
+            className={css`
+              width: ${10 * FONT_WIDTH * FONT_SCALE_FACTOR}px;
+            `}
+          >
+            <Text>Owned by:</Text>
+            <div>
+              <Text> </Text>
+            </div>
+            <Text>
+              {owner
+                ? ethers.utils.hexZeroPad(owner, 20).replace(/^0x/, '').toUpperCase()
+                : undefined}
+            </Text>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return { props: { token: context.query.token } }
 }
