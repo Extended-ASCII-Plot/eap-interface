c145fa2791fff5cc4f6547ae14965b9cf8b318e6
Found Pan Tiger <renzholy@gmail.com>
2021-11-23 20:32:38 +0800

    auto connect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -97,7 +97,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <RenderProvider value={comlinkWorkerRef.current}>
         <ContractProvider value={contract}>
-          <UseWalletProvider>
+          <UseWalletProvider autoConnect={true}>
             <Component {...pageProps} />
           </UseWalletProvider>
         </ContractProvider>
