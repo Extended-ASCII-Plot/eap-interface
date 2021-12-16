0962857da20cd69ba8cff28ab5d5567fc60a2bb9
Found Pan Tiger <renzholy@gmail.com>
2021-12-16 15:56:37 +0800

    deps: upgrade

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -97,7 +97,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <RenderProvider value={comlinkWorkerRef.current}>
         <ContractProvider value={contract}>
-          <UseWalletProvider autoConnect={true}>
+          <UseWalletProvider>
             <Component {...pageProps} />
           </UseWalletProvider>
         </ContractProvider>
